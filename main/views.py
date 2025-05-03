a9be6a9260a3e390a93b00823bd5831f6bea9b30
dinu2854i <dinureddyreddy143@gmail.com>
2025-05-03 08:08:13 +0530

    Updates

--- main/views.py
+++ main/views.py
@@ -32,7 +32,7 @@ def contact(request):
              
     return render(request, 'contact.html')
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 def about(request):
     return render(request, 'about.html')
 
